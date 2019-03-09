4edbda882ad68b8529e9446c2416d9612d87e457
aasik-123 <47380412+aasik-123@users.noreply.github.com>
2019-03-09 01:50:55 -0800

    Create cha14.java

---
+++ cha14.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,i,k;
+		Scanner s=new Scanner(System.in);
+		k=s.nextInt();
+		n=s.nextInt();
+		int arr[]=new int[1000];
+		for(i=k+1;i<n;i++)
+		{
+			if(i%2!=0)
+			{
+				System.out.print(i+" ");
+			}}}
